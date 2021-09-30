5f0f642802784a15e4faa3493b470b280208f12b
root <root@DESKTOP-E202IP4.localdomain>
2021-09-30 19:29:17 -0300

    upload file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/UseCases/createCategory";
+import { importCategoryController } from "../modules/cars/UseCases/importCategory";
 import { listCategoryController } from "../modules/cars/UseCases/listCategory";
 
 const categoriesRouter = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRouter.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
-
 categoriesRouter.get("/", (request, response) => {
   return listCategoryController.handle(request, response);
 });
 
+categoriesRouter.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
+});
 export { categoriesRouter };
